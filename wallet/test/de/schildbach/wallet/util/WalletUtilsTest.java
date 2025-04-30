21c55e1e04f738bbe10f8e0257f27b87ee280dc0
X <134867140+Perrett411@users.noreply.github.com>
2025-04-30 19:06:40 -0400

    Update WalletUtilsTest.java

--- wallet/test/de/schildbach/wallet/util/WalletUtilsTest.java
+++ wallet/test/de/schildbach/wallet/util/WalletUtilsTest.java
@@ -39,3 +39,12 @@ public class WalletUtilsTest {
                 MainNetParams.get());
     }
 }
+# Enter the associate accountant details
+name = "ASSOCIATE accountant"
+owner = "Perrett and
+Associates Private Investment Firm LLC"
+business_relation = "associate to Perrett and Associates"
+association = "financial management"
+ai_name = "Perrett and Associates CFO AI"
+CEO = Christopher L Perrett 
+Define a function called `handle_multiply_speakers` that takes in a `conversation` (represented as a list of speaker's inputs) and a `speakers` list, then perform the following steps: Analyzed voice 
